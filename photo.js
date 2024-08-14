24194666b887c61336ee9f251b6a223238a503c7
Remi bonin <rbonin.rbonin@gmail.com>
2024-08-13 21:29:33 -0400

    Update photo.js, plus.html and seven other files

---
+++ photo.js
@@ -0,0 +1,53 @@
+// Get all the images
+image_array = [
+    
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpeg",
+    "6.jpeg",
+    "7.jpeg",
+    "8.jpeg",
+    "9.jpeg",
+    "10.jpeg",
+        "11.jpeg",
+    "12.jpeg",
+    "13.jpeg",
+    "14.jpeg",
+    "15.jpeg",
+    "16.jpeg",
+    "17.jpeg",
+    "18.jpeg",
+    "19.jpeg",
+    "20.jpeg",
+      "21.jpeg",
+    "22.jpeg",
+    "23.jpeg",
+    "24.jpeg",
+    "25.jpeg",
+    "26.jpeg",
+    "27.jpeg",
+    "28.jpeg",
+    "29.jpeg",
+    "30.jpeg",
+    "31.jpeg",
+    "32.jpeg",
+       "33.jpeg",
+    "34.jpeg",
+    "35.jpeg",
+    "36.jpeg",
+    "37.jpeg",
+  ]
+  
+  function get_random_image(){
+    // Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+  
+    // Get an image at the random_index
+    selected_image = image_array[random_index]
+  
+    // Display the image
+    document.getElementById('random_image').src = `./photo/Bert/${selected_image}`
+  }
+  
