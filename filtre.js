5d7e9afd82abbd9a06aef903a2aa183291c43544
Truiteetthon <92497471+Truiteetthon@users.noreply.github.com>
2024-08-31 10:40:12 -0400

    new

---
+++ filtre.js
@@ -0,0 +1,8 @@
+
+    var checkBox = document.getElementById("Photographie");
+    var box = document.getElementById("photo");
+    if (checkBox.checked == true){
+      box.style.display = "block";
+    } else {
+       box.style.display = "none";
+    }
