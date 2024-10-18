c7a374ef18c9130ca03e7d22588968fadd4772b2
Truiteetthon <92497471+Truiteetthon@users.noreply.github.com>
2024-10-18 00:14:36 -0400

    micro

--- Javascript/p1.js
+++ Javascript/p1.js
@@ -1,32 +1,33 @@
 
-const P13Images = [
-'photo/P13/P13-1.jpg',
-'photo/P13/P13-2.jpg',
-'photo/P13/P13-3.jpg',
-'photo/P13/P13-4.jpg',
-'photo/P13/P13-5.jpg',
+const P14Images = [
+'photo/P14/P14-1.jpg',
+'photo/P14/P14-2.jpg',
+'photo/P14/P14-3.jpg',
+'photo/P14/P14-4.jpg',
+'photo/P14/P14-5.jpg',
+'photo/P14/P14-6.jpg',
 ];
 
 
-// P13 ---------------------------
-let P13currentIndex = 0;
+// P14 ---------------------------
+let P14currentIndex = 0;
 
 // Get the image element
-const P13 = document.getElementById('P13');
+const P14 = document.getElementById('P14');
 
 // Function to change the image
-const P13changeImage = () => {
+const P14changeImage = () => {
   // Increment the index to point to the next image
-  P13currentIndex++;
+  P14currentIndex++;
 
   // If the index goes beyond the array length, reset it to 0 (loop back to the first image)
-  if (P13currentIndex >= P13Images.length) {
-      P13currentIndex = 0;
+  if (P14currentIndex >= P14Images.length) {
+      P14currentIndex = 0;
   }
 
   // Update the image source to the new image
-  P13.src = P13Images[P13currentIndex];
+  P14.src = P14Images[P14currentIndex];
 };
 
 // Add event listeners for both click and touchstart
-P13.addEventListener('click', P13changeImage);
+P14.addEventListener('click', P14changeImage);
--- Javascript/p11.js
+++ Javascript/p11.js
@@ -67,6 +67,8 @@ const P13Images = [
   'photo/P13/P13-3.jpg',
   'photo/P13/P13-4.jpg',
   'photo/P13/P13-5.jpg',
+  'photo/P13/P13-6.jpg',
+  'photo/P13/P13-7.jpg',
   ];
 
 const BertImages = [
@@ -83,6 +85,16 @@ const BertImages = [
 'photo/Bert/Bert-11.jpg',
 'photo/Bert/Bert-12.jpg',
 ];
+
+const P14Images = [
+  'photo/P14/P14-1.jpg',
+  'photo/P14/P14-2.jpg',
+  'photo/P14/P14-3.jpg',
+  'photo/P14/P14-4.jpg',
+  'photo/P14/P14-5.jpg',
+  'photo/P14/P14-6.jpg',
+  ];
+  
 
 // Array of I ------------------------------------------
 
@@ -311,7 +323,28 @@ const P13changeImage = () => {
 P13.addEventListener('click', P13changeImage);
 
 
+// P14 ---------------------------
+let P14currentIndex = 0;
 
+// Get the image element
+const P14 = document.getElementById('P14');
+
+// Function to change the image
+const P14changeImage = () => {
+  // Increment the index to point to the next image
+  P14currentIndex++;
+
+  // If the index goes beyond the array length, reset it to 0 (loop back to the first image)
+  if (P14currentIndex >= P14Images.length) {
+      P14currentIndex = 0;
+  }
+
+  // Update the image source to the new image
+  P14.src = P14Images[P14currentIndex];
+};
+
+// Add event listeners for both click and touchstart
+P14.addEventListener('click', P14changeImage);
 
 
 // Bert ---------------------------
