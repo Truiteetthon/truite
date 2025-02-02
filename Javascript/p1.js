7dd9596fae36ba3837190cdafb12b2fee7e305fc
Truiteetthon <92497471+Truiteetthon@users.noreply.github.com>
2025-02-02 17:05:10 -0500

    Kazuyo Sejima

--- Javascript/p1.js
+++ Javascript/p1.js
@@ -1,35 +1,33 @@
 
-const P15Images = [
-'photo/P15/P15-1.jpg',
-'photo/P15/P15-2.jpg',
-'photo/P15/P15-3.jpg',
-'photo/P15/P15-4.jpg',
-'photo/P15/P15-5.jpg',
-'photo/P15/P15-6.jpg',
-'photo/P15/P15-7.jpg',
-'photo/P15/P15-8.jpg',
+const I6Images = [
+'photo/I6/I6-1.jpg',
+'photo/I6/I6-2.jpg',
+'photo/I6/I6-3.jpg',
+'photo/I6/I6-4.jpg',
+'photo/I6/I6-5.jpg',
+'photo/I6/I6-6.jpg',
 ];
 
 
-// P15 ---------------------------
-let P15currentIndex = 0;
+// I6 ---------------------------
+let I6currentIndex = 0;
 
 // Get the image element
-const P15 = document.getElementById('P15');
+const I6 = document.getElementById('I6');
 
 // Function to change the image
-const P15changeImage = () => {
+const I6changeImage = () => {
   // Increment the index to point to the next image
-  P15currentIndex++;
+  I6currentIndex++;
 
   // If the index goes beyond the array length, reset it to 0 (loop back to the first image)
-  if (P15currentIndex >= P15Images.length) {
-      P15currentIndex = 0;
+  if (I6currentIndex >= I6Images.length) {
+      I6currentIndex = 0;
   }
 
   // Update the image source to the new image
-  P15.src = P15Images[P15currentIndex];
+  I6.src = I6Images[I6currentIndex];
 };
 
 // Add event listeners for both click and touchstart
-P15.addEventListener('click', P15changeImage);
+I6.addEventListener('click', I6changeImage);
--- Javascript/p11.js
+++ Javascript/p11.js
@@ -129,7 +129,14 @@ const I3Images = [
     'photo/I4/I4-4.jpg',
     ];
 
-
+const I6Images = [
+'photo/I6/I6-2.jpg',
+'photo/I6/I6-1.jpg',
+'photo/I6/I6-3.jpg',
+'photo/I6/I6-4.jpg',
+'photo/I6/I6-5.jpg',
+'photo/I6/I6-6.jpg',
+];
 // Code of P --------------------------------------
   
 // P1 -----------------
@@ -469,3 +476,28 @@ const I4changeImage = () => {
 
 // Add event listeners for both click and touchstart
 I4.addEventListener('click', I4changeImage);
+
+
+
+// I6 ---------------------------
+let I6currentIndex = 0;
+
+// Get the image element
+const I6 = document.getElementById('I6');
+
+// Function to change the image
+const I6changeImage = () => {
+  // Increment the index to point to the next image
+  I6currentIndex++;
+
+  // If the index goes beyond the array length, reset it to 0 (loop back to the first image)
+  if (I6currentIndex >= I6Images.length) {
+      I6currentIndex = 0;
+  }
+
+  // Update the image source to the new image
+  I6.src = I6Images[I6currentIndex];
+};
+
+// Add event listeners for both click and touchstart
+I6.addEventListener('click', I6changeImage);
